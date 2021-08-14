3b1253ea2d2641c49c028e9596cc365b79ac31d4
Chrono <chrono_legionnaire@protonmail.com>
2021-08-14 13:21:40 +0800

    Add files via upload

---
+++ Helper/ClientMySQLDBConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Runtime.InteropServices;
+using ASodium;
+
+namespace PriSecDBAPI.Helper
+{
+    public class ClientMySQLDBConnection
+    {
+        //server=localhost;port=3306;database=databasename;user=databaseusername;password=databaseuserpassword;
+        public MySqlConnection ClientMySQLConnection = new MySqlConnection();
+        public Boolean CheckConnection;
+        public String ConnectionString = "server=localhost;port=3306;";
+
+        public void SetConnection(String DBName, String DBUserName, String DBPassword) 
+        {
+
+            ConnectionString += "database=" + DBName + ";" + "user=" + DBUserName + ";" + "password=" + DBPassword + ";";
+
+            ClientMySQLConnection.ConnectionString = ConnectionString;
+        }
+
+        public Boolean LoadConnection(String DBName, String DBUserName, String DBPassword,ref String Exception)
+        {
+            SetConnection(DBName,DBUserName,DBPassword);
+            try
+            {
+                ClientMySQLConnection.Open();
+                CheckConnection = true;
+            }
+            catch (MySqlException exception)
+            {
+                CheckConnection = false;
+                Exception = exception.ToString();
+            }
+            return CheckConnection;
+        }
+    }
+}
