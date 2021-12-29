0023c13e1644575186e3250278685ce15f48284e
Chrono <chrono_legionnaire@protonmail.com>
2021-12-29 14:56:37 +0800

    Add files via upload

---
+++ V2/Model/NormalDBUpdateModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PriSecDBAPI.Model
+{
+    public class NormalDBUpdateModel
+    {
+        public SealedDBCredentialModel MyDBCredentialModel { get; set; }
+
+        public String UniquePaymentID { get; set; }
+
+        public String Base64QueryString { get; set; }
+
+        public String[] Base64ParameterName { get; set; }
+
+        public String[] Base64ParameterValue { get; set; }
+
+        public String IDUsedToUpdate { get; set; }
+
+        public Boolean IsXSalsa20Poly1305 { get; set; }
+
+    }
+}
